c9f609f0202ea54ef64b7f140d955fdbb66632bb
Davide Galante <Davidegalante88@gmail.com>
2026-02-06 21:07:59 +0100

    Aggiunta configurazione Capacitor e GitHub Actions

---
+++ android/app/src/main/java/com/dave/myconcerts/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dave.myconcerts;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dave.myconcerts',
+  appName: 'My Concerts',
+  webDir: 'dist'
+};
+
+export default config;
